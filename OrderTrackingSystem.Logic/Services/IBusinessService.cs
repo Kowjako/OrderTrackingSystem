0a3a9764a93a38bf15af3df9a4cf38b44022a048
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-01-08 11:43:25 +0100

    Add interface for further BusinessServices

---
+++ OrderTrackingSystem.Logic/Services/IBusinessService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Logic.Services
+{
+    public interface IBusinessService<T>
+    {
+        ICollection<T> GetByPrimary(int id);
+        ICollection<T> GetAll();
+
+        void Add(T obj);
+        void Remove(int id);
+        void Update(T obj);
+
+        T CreateNewInstance();
+    }
+}
