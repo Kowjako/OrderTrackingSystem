8af99f851d2965145fb9c464baf3ffe4796cb40d
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-02-06 16:12:52 +0100

    Further implementations for mindmap control

---
+++ OrderTrackingSystem/CustomControls/MindMapControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderTrackingSystem.Presentation.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for MindMapControl.xaml
+    /// </summary>
+    public partial class MindMapControl : UserControl
+    {
+        public MindMapControl()
+        {
+            InitializeComponent();
+        }
+
+        private void expandBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if(btn != null)
+            {
+                btn.Content = btn.Content.Equals("+") ? "-" : "+";
+            }
+        }
+    }
+}
