f170dbdf7362f25b8146c3e69cb6c5f77e127ae4
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-02-09 20:28:28 +0100

    linked elements drawing logic completed - connectors next

--- OrderTrackingSystem/CustomControls/MindMapControl.xaml.cs
+++ OrderTrackingSystem/CustomControls/MindMapControl.xaml.cs
@@ -12,7 +12,7 @@ namespace OrderTrackingSystem.Presentation.CustomControls
     /// </summary>
     public partial class MindMapControl : UserControl
     {
-        ICollection<string> LinkedItems = new List<string>();
+        IList<string> LinkedItems = new List<string>();
 
         public MindMapControl()
         {
@@ -29,8 +29,9 @@ namespace OrderTrackingSystem.Presentation.CustomControls
             if(LinkedItems.Count == 0)
             {
                 DrawMainConnector();
-                LinkedItems.Add(refNumber);
             }
+            LinkedItems.Add(refNumber);
+            OnNodeAdded(refNumber);
         }
 
         private void DrawMainConnector()
@@ -52,8 +53,98 @@ namespace OrderTrackingSystem.Presentation.CustomControls
             Grid.SetColumn(rectangle, 2);
             helpGrid.Children.Add(rectangle);
 
-            Grid.SetColumn(secondaryRectangle, 2);
+            Grid.SetColumn(secondaryRectangle, 0);
             secondaryGrid.Children.Add(secondaryRectangle);
+        }
+
+        private void DrawOrderElements()
+        {
+            for(int i = 0; i < LinkedItems.Count; i++)
+            {
+                var border = new Border()
+                {
+                    CornerRadius = new CornerRadius(15),
+                    BorderBrush = new SolidColorBrush { Color = Colors.Maroon },
+                    BorderThickness = new Thickness(3),
+                    Margin = new Thickness(0, 5, 5, 5),
+                };
+
+                var header = new TextBlock()
+                {
+                    Text = LinkedItems[i],
+                    FontWeight = FontWeights.Bold,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                border.Child = header;
+
+                var rowNumber = CalculateRowNumber(i);
+                Grid.SetRow(border, rowNumber);
+                //DrawHelperConnector(rowNumber);
+                ordersGrid.Children.Add(border);
+            }
+        }
+
+        private void OnNodeAdded(string refNumber)
+        {
+            BuildDefaultGrid();
+            ordersGrid.Children.Clear();
+            DrawOrderElements();
+        }
+        
+        private int CalculateRowNumber(int elementIndex)
+        {
+            if (LinkedItems.Count == 1)
+            {
+                return 1;
+            }
+            else 
+                return elementIndex;
+        }
+
+        private void DrawHelperConnector(int rowNumber)
+        {
+            if (LinkedItems.Count == 1) return;
+
+            var line = new Line
+            {
+                Stroke = new SolidColorBrush { Color = Colors.Black },
+                StrokeThickness = 2,
+                Tag = "Connector"
+            };
+
+            line.X1 = 0;
+            line.Y1 = secondaryGrid.ActualHeight / 2;
+
+            line.X2 = secondaryGrid.ColumnDefinitions[0].ActualWidth;
+            if (rowNumber == 2)
+            {
+                line.Y2 = secondaryGrid.ActualHeight - ordersGrid.RowDefinitions[rowNumber].ActualHeight / 2;
+            }
+            else
+            {
+                line.Y2 = ordersGrid.RowDefinitions[rowNumber].ActualHeight / 2;
+            }
+
+            secondaryGrid.Children.Add(line);
+        }
+
+        private void BuildDefaultGrid()
+        {
+            if (LinkedItems.Count == 2)
+            {
+                ordersGrid.RowDefinitions.Clear();
+                ordersGrid.RowDefinitions.Add(new RowDefinition());
+                ordersGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            else
+            {
+                if (LinkedItems.Count == 3) ordersGrid.RowDefinitions.Clear();
+                ordersGrid.RowDefinitions.Add(new RowDefinition());
+                ordersGrid.RowDefinitions.Add(new RowDefinition());
+                ordersGrid.RowDefinitions.Add(new RowDefinition());
+            }
         }
     }
 }
