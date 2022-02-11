aa86b1ae619a39e528b52f1fcd35e7ce1f6726e7
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-02-11 15:32:06 +0100

    Worked MindMap control

--- OrderTrackingSystem/CustomControls/MindMapControl.xaml.cs
+++ OrderTrackingSystem/CustomControls/MindMapControl.xaml.cs
@@ -26,7 +26,8 @@ namespace OrderTrackingSystem.Presentation.CustomControls
 
         public void AddNode(string refNumber)
         {
-            if(LinkedItems.Count == 0)
+            if (LinkedItems.Count == 3) return;
+            if (LinkedItems.Count == 0)
             {
                 DrawMainConnector();
             }
@@ -43,28 +44,27 @@ namespace OrderTrackingSystem.Presentation.CustomControls
                 Fill = new SolidColorBrush { Color = Colors.Black}
             };
 
-            var secondaryRectangle = new Rectangle
-            {
-                VerticalAlignment = VerticalAlignment.Center,
-                Height = 2,
-                Fill = new SolidColorBrush { Color = Colors.Black }
-            };
-
             Grid.SetColumn(rectangle, 2);
             helpGrid.Children.Add(rectangle);
-
-            Grid.SetColumn(secondaryRectangle, 0);
-            secondaryGrid.Children.Add(secondaryRectangle);
         }
 
         private void DrawOrderElements()
         {
-            for(int i = 0; i < LinkedItems.Count; i++)
+            /* Usuwamy poprzednie polaczenia */
+            var copy = secondaryGrid.Children.Count; /* tworzymy value-type kopie */
+            for (int i = 1; i < copy; i++)
+            {
+                secondaryGrid.Children.RemoveAt(1);
+            }
+
+            for (int i = 0; i < LinkedItems.Count; i++)
             {
                 var border = new Border()
                 {
                     CornerRadius = new CornerRadius(15),
-                    BorderBrush = new SolidColorBrush { Color = Colors.Maroon },
+                    BorderBrush = new SolidColorBrush { Color = i == 0 ? Colors.Red : 
+                                                                i == 1 ? Colors.Green :
+                                                                Colors.Blue},
                     BorderThickness = new Thickness(3),
                     Margin = new Thickness(0, 5, 5, 5),
                 };
@@ -80,10 +80,11 @@ namespace OrderTrackingSystem.Presentation.CustomControls
                 border.Child = header;
 
                 var rowNumber = CalculateRowNumber(i);
+                
                 Grid.SetRow(border, rowNumber);
-                //DrawHelperConnector(rowNumber);
                 ordersGrid.Children.Add(border);
             }
+            DrawHelperConnector();
         }
 
         private void OnNodeAdded(string refNumber)
@@ -103,31 +104,74 @@ namespace OrderTrackingSystem.Presentation.CustomControls
                 return elementIndex;
         }
 
-        private void DrawHelperConnector(int rowNumber)
+        private void DrawHelperConnector()
         {
-            if (LinkedItems.Count == 1) return;
-
-            var line = new Line
+            if (LinkedItems.Count == 1 | LinkedItems.Count == 3)
             {
-                Stroke = new SolidColorBrush { Color = Colors.Black },
-                StrokeThickness = 2,
-                Tag = "Connector"
-            };
+                var line = new Rectangle
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Height = 2,
+                    Fill = new SolidColorBrush { Color = Colors.Black }
+                };
 
-            line.X1 = 0;
-            line.Y1 = secondaryGrid.ActualHeight / 2;
-
-            line.X2 = secondaryGrid.ColumnDefinitions[0].ActualWidth;
-            if (rowNumber == 2)
-            {
-                line.Y2 = secondaryGrid.ActualHeight - ordersGrid.RowDefinitions[rowNumber].ActualHeight / 2;
+                Grid.SetColumn(line, 0);
+                secondaryGrid.Children.Add(line);
             }
-            else
+            if (LinkedItems.Count != 1)
             {
-                line.Y2 = ordersGrid.RowDefinitions[rowNumber].ActualHeight / 2;
-            }
+                #region Vertical Connector
 
-            secondaryGrid.Children.Add(line);
+                var line2 = new Rectangle
+                {
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Height = 1.5,
+                    Fill = new SolidColorBrush { Color = Colors.Black }
+                };
+
+                line2.Width = secondaryGrid.ActualHeight - ordersGrid.RowDefinitions[0].ActualHeight / 2;
+
+                /* Ustawiamy obrot + dublowanie */
+                var transformGroup = new TransformGroup();
+                var rotateTransform = new RotateTransform() { Angle = 270 };
+                var scaleTransform = new ScaleTransform() { ScaleY = -1 };
+
+                transformGroup.Children.Add(rotateTransform);
+                transformGroup.Children.Add(scaleTransform);
+
+                line2.LayoutTransform = transformGroup;
+
+                Grid.SetColumn(line2, 0);
+                secondaryGrid.Children.Add(line2);
+
+                #endregion
+
+                #region Horizontal Connector
+
+                var topConnector = new Rectangle
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Height = 2,
+                    Fill = new SolidColorBrush { Color = Colors.Black }
+                };
+
+                topConnector.Margin = new Thickness(0, 0, 0, line2.Width);
+                Grid.SetColumn(topConnector, 0);
+                secondaryGrid.Children.Add(topConnector);
+
+                var bottomConnector = new Rectangle
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Height = 2,
+                    Fill = new SolidColorBrush { Color = Colors.Black }
+                };
+
+                bottomConnector.Margin = new Thickness(0, line2.Width, 0, 0);
+                Grid.SetColumn(bottomConnector, 0);
+                secondaryGrid.Children.Add(bottomConnector);
+
+                #endregion
+            }
         }
 
         private void BuildDefaultGrid()
