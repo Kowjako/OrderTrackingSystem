6e5523c881d8daa204956400a39c87341ba9a01a
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-02-28 13:08:11 +0100

    Re-worked mailbox view

--- OrderTrackingSystem/Properties/AssemblyInfo.cs
+++ OrderTrackingSystem/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OrderTrackingSystem")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Włodzimierz Kowjako ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- OrderTrackingSystem/Views/MailBoxView.xaml.cs
+++ OrderTrackingSystem/Views/MailBoxView.xaml.cs
@@ -1,19 +1,7 @@
-﻿using OrderTrackingSystem.Presentation.CustomControls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrderTrackingSystem.Presentation.Views
 {
@@ -26,25 +14,12 @@ namespace OrderTrackingSystem.Presentation.Views
         public MailBoxView()
         {
             InitializeComponent();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var popup = new Popup
-            {
-                Width = 400,
-                Child = new Notifyer(),
-                AllowsTransparency = true,
-                PopupAnimation = PopupAnimation.Slide,
-                PlacementTarget = mainGrid,
-                Placement = PlacementMode.Custom,
-                StaysOpen = false
-            };
-            popup.CustomPopupPlacementCallback = (ppSize, tgSize, pointX) =>
-            {
-                return new[] { new CustomPopupPlacement(new Point(mainGrid.ActualWidth - (popup.Child as Notifyer).ActualWidth - 15,15), PopupPrimaryAxis.Horizontal) };
-            };
-            popup.IsOpen = true;
+            WindowExtension.WindowExtension.WithNotifying(this, CustomControls.NotifyType.Success, mainGrid);
         }
     }
 }
