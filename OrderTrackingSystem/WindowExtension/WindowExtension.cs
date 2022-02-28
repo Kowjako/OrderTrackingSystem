688d6595021d94da3bf03ba347083f234b4db113
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-02-28 13:08:27 +0100

    Add notifyer extension for UserControl class

---
+++ OrderTrackingSystem/WindowExtension/WindowExtension.cs
@@ -0,0 +1,16 @@
+﻿using OrderTrackingSystem.Presentation.CustomControls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OrderTrackingSystem.Presentation.WindowExtension
+{
+    public static class WindowExtension
+    {
+        /* Extension-method dla klasy Window dla uzycia Notifyer'a */
+        public static Notifyer Notifyer = new Notifyer();
+        public static void WithNotifying<T>(T window, NotifyType type, FrameworkElement mainContentControl) where T:UserControl
+        {
+            Notifyer.ShowNotifyer(type, mainContentControl);
+        }
+    }
+}
