46485e8e03db0f46e45591b8f22589ec5a2c644a
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-03-05 19:25:26 +0100

    Add orderService and enummapper

---
+++ OrderTrackingSystem.Logic/EnumMappers/EnumMapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Logic.EnumMappers
+{
+    public enum PayType
+    {
+        Gotowka = 0,
+        ApplePay = 1,
+        Karta = 2,
+        BLIK = 3
+    }
+
+    public static class PayTypeEnumConverter
+    {
+        public static string GetNameById(int id)
+        {
+            switch(id)
+            {
+                case 0:
+                    return "Gotowka";
+                case 1:
+                    return "ApplePay";
+                case 2:
+                    return "Karta";
+                case 3:
+                    return "BLIK";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+
+}
---
+++ OrderTrackingSystem.Logic/Services/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using OrderTrackingSystem.Logic.DataAccessLayer;
+using OrderTrackingSystem.Logic.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using OrderTrackingSystem.Logic.EnumMappers;
+using System.Data.Entity;
+
+namespace OrderTrackingSystem.Logic.Services
+{
+    public class OrderService : IService<OrderService>
+    {
+        public async Task<List<OrderDTO>> GetOrdersForCustomer(int customerId)
+        {
+            using (var dbContext = new OrderTrackingSystemEntities())
+            {
+                var customer = await dbContext.Customers.Where(c => c.Id == customerId).FirstAsync();
+                /* ladujemy kolekcje zamowien dla danego kleinta */
+                await dbContext.Entry(customer).Collection(nameof(customer.Orders)).LoadAsync();
+
+                var query = from order in customer.Orders
+                            let valueQuery = (from cart in dbContext.OrderCarts
+                                              from prodcut in dbContext.Products.Where(p => p.Id == cart.ProductId).DefaultIfEmpty()
+                                              where cart.OrderId == order.Id
+                                              select cart.Amount * prodcut.PriceBrutto).Sum()
+                            let sellerQuery = from cart in dbContext.OrderCarts
+                                              from prodcut in dbContext.Products.Where(p => p.Id == cart.ProductId).DefaultIfEmpty()
+                                              from seller in dbContext.Sellers.Where(p => p.Id == prodcut.SellerId).DefaultIfEmpty()
+                                              where cart.OrderId == order.Id
+                                              select seller.Name
+                            select new OrderDTO
+                            {
+                                Numer = order.Number,
+                                Oplata = PayTypeEnumConverter.GetNameById(order.Id),
+                                Sklep = sellerQuery.FirstAsync().Result,
+                                Dostawa = order.DeliveryType,
+                                Rezygnacja = order.ComplaintDefinitionId != null,
+                                Kwota = string.Format("{0:0.00 zł}", valueQuery)
+                            };
+
+                return query.ToList();
+            }
+        }
+    }
+}
