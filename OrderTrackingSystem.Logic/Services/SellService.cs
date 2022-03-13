4e2f6aa0e4ae1088fd460f3fe6cfebe12e0b7f56
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-03-13 18:18:00 +0100

    Implemented fetching sells list for customer

--- OrderTrackingSystem.Logic/Services/SellService.cs
+++ OrderTrackingSystem.Logic/Services/SellService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using OrderTrackingSystem.Logic.DataAccessLayer;
+using OrderTrackingSystem.Logic.DTO;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OrderTrackingSystem.Logic.Services
 {
-    class SellService
+    public class SellService : IService<SellService>
     {
+        public async Task<List<SellDTO>> GetSellsForCustomer(int customerId)
+        {
+            using (var dbContext = new OrderTrackingSystemEntities())
+            {
+                var customer = await dbContext.Customers.Where(c => c.Id == customerId).FirstAsync();
+                await dbContext.Entry(customer).Collection(nameof(customer.Sells)).LoadAsync();
+
+                var query = from sells in customer.Sells
+                            let valueQuery = (from cart in dbContext.SellCarts
+                                              from prodcut in dbContext.Products.Where(p => p.Id == cart.ProductId).DefaultIfEmpty()
+                                              where cart.SellId == sells.Id
+                                              select cart.Amount * prodcut.PriceBrutto).Sum()
+                            let receiverQuery = from receiver in dbContext.Customers
+                                                where receiver.Id == sells.CustomerId
+                                                select receiver.Name + " " + receiver.Surname
+                            select new SellDTO
+                            {
+                                Numer = sells.Number,
+                                Data = sells.SellingDate.ToShortDateString(),
+                                Dni = sells.PickupDays.ToString(),
+                                Odbiorca = receiverQuery.FirstAsync().Result,
+                                Kwota = string.Format("{0:0.00 zł}", valueQuery)
+                            };
+
+                return query.ToList();
+            }
+        }
     }
 }
--- OrderTrackingSystem/Views/CurrentAccountView.xaml.cs
+++ OrderTrackingSystem/Views/CurrentAccountView.xaml.cs
@@ -1,18 +1,5 @@
-﻿using OrderTrackingSystem.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using OrderTrackingSystem.Presentation.WindowExtension;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrderTrackingSystem.Views
 {
@@ -24,6 +11,11 @@ namespace OrderTrackingSystem.Views
         public CurrentAccountView()
         {
             InitializeComponent();
+        }
+
+        private void sellsGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            DisplayNameBinder.SetDisplayNameIfExists(e);
         }
     }
 }
