478529b023a228c19863cb73f49cf1defcce21a5
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-03-13 18:17:05 +0100

    Add displaynamebinder implementation for DTO object representation

---
+++ OrderTrackingSystem/WindowExtension/DisplayNameBinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace OrderTrackingSystem.Presentation.WindowExtension
+{
+    public class DisplayNameBinder
+    {
+        public static void SetDisplayNameIfExists(DataGridAutoGeneratingColumnEventArgs e)
+        {
+            var displayName = GetPropertyDisplayName(e.PropertyDescriptor);
+            if (!string.IsNullOrEmpty(displayName))
+            {
+                e.Column.Header = displayName;
+            }
+        }
+
+        public static string GetPropertyDisplayName(object descriptor)
+        {
+            var pd = descriptor as PropertyDescriptor;
+            if (pd != null)
+            {
+                DisplayNameAttribute dn = pd.Attributes[typeof(DisplayNameAttribute)] as DisplayNameAttribute;
+                if (dn != null && dn != DisplayNameAttribute.Default)
+                {
+                    return dn.DisplayName;
+                }
+            }
+            else
+            {
+                var pi = descriptor as PropertyInfo;
+                if (pi != null)
+                {
+                    Object[] attributes = pi.GetCustomAttributes(typeof(DisplayNameAttribute), true);
+                    for (int i = 0; i < attributes.Length; ++i)
+                    {
+                        DisplayNameAttribute dn = attributes[i] as DisplayNameAttribute;
+                        if (dn != null && dn != DisplayNameAttribute.Default)
+                        {
+                            return dn.DisplayName;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
