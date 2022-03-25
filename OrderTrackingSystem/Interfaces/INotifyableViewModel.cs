f6d98ac4d8f9a857d118d93e2b8380a6c783d7d4
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-03-25 18:55:44 +0100

    Add INotifyableViewModel for further event bindings with notifyer

---
+++ OrderTrackingSystem/Interfaces/INotifyableViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Presentation.Interfaces
+{
+    public interface INotifyableViewModel
+    {
+        event Action<string> OnSuccess;
+        event Action<string> OnFailure;
+        event Action<string> OnWarning;
+    }
+}
