e591b493dfd52930e61a31e470d103ab8894f660
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-03-31 18:26:31 +0200

    Add decimalToString value converter

---
+++ OrderTrackingSystem/ValueConverter/ValueConverters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OrderTrackingSystem.Presentation.ValueConverter
+{
+    [ValueConversion(typeof(decimal), typeof(string))]
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            /* Pattern matching */
+            if (value is decimal decimalValue && decimalValue == 0m)
+                return string.Empty;
+            else
+                return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal decimalValue = decimal.Parse(value as string, CultureInfo.InvariantCulture);
+            if(decimalValue != 0m)
+            {
+                return decimalValue;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
