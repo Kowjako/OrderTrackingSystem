c906e142cf7967ba3afb32706726a59a289d1d23
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-03 11:11:04 +0200

    Add mailDTO

---
+++ OrderTrackingSystem.Logic/DTO/MailDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace OrderTrackingSystem.Logic.DTO
+{
+    public class MailDTO
+    {
+        [Browsable(false)]
+        public int Id { get; set; }
+        [DisplayName("Tytuł")]
+        public string Caption { get; set; }
+        [DisplayName("Treść")]
+        public string Content { get; set; }
+        public string Nadawca { get; set; }
+        public string Odbiorca { get; set; }
+        [Browsable(false)]
+        public string[] RelatedOrders { get; set; }
+    }
+}
