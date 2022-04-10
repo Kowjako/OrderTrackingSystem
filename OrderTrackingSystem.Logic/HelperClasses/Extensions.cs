f2bca564b7ba69879e71e8aef4fb74901c6edc70
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-10 18:21:04 +0200

    Add ForEachAsync extension method to avoid EF problems

---
+++ OrderTrackingSystem.Logic/HelperClasses/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Logic.HelperClasses
+{
+    public static class Extensions
+    {
+        public async static Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> func)
+        {
+            foreach(var item in items)
+            {
+                await func(item);
+            }
+        }
+    }
+}
