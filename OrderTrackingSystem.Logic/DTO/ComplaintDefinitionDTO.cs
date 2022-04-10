67a4054eafc32a1619c1673f1d5fdc033c3a15f5
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-10 22:09:59 +0200

    Add complaintfolder dto + included composite pattern

--- OrderTrackingSystem.Logic/DTO/ComplaintDefinitionDTO.cs
+++ OrderTrackingSystem.Logic/DTO/ComplaintDefinitionDTO.cs
@@ -12,5 +12,17 @@ namespace OrderTrackingSystem.Logic.DTO
         public string Name { get; set; }
         [Display(Name = "RemainDays", ResourceType = typeof(Properties.Resources))]
         public string RemainDays { get; set; }
+        [Display(Name = "Description", ResourceType = typeof(Properties.Resources))]
+        public string Definition { get; set; }
+    }
+
+    public class ComplaintFolderDTO
+    {
+        [Browsable(false)]
+        public int Id { get; set; }
+        [Display(Name = "UniqueName", ResourceType = typeof(Properties.Resources))]
+        public string Name { get; set; }
+        [Browsable(false)]
+        public List<ComplaintFolderDTO> Children { get; set; }
     }
 }
