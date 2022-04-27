4fec7dddd00fe854791d97c688dc260d52ddce2c
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-27 16:52:32 +0200

    Re-worked and binded login window

--- OrderTrackingSystem/Views/LoginWindow.xaml.cs
+++ OrderTrackingSystem/Views/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderTrackingSystem.Presentation.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace OrderTrackingSystem.Presentation.Views
         public LoginWindow()
         {
             InitializeComponent();
+            DataContext = new StartupViewModel();
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -30,14 +32,36 @@ namespace OrderTrackingSystem.Presentation.Views
             this.DragMove();
         }
 
-        private void createNewAcc_Click(object sender, RoutedEventArgs e)
+        private void createAcc_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if(sender is TextBlock tb && tb.Tag.Equals("customer"))
+            {
+                sellerGrid.Width = 0;
+                customerGrid.Width = 190;
+                (DataContext as StartupViewModel).CreationForClient = true;
+            }
+            else
+            {
+                sellerGrid.Width = 190;
+                customerGrid.Width = 0;
+                (DataContext as StartupViewModel).CreationForClient = false;
+            }
             var gridAnimation = new DoubleAnimation();
             gridAnimation.From = 800;
             gridAnimation.To = 1180;
             gridAnimation.Duration = TimeSpan.FromSeconds(2);
             gridAnimation.EasingFunction = new CircleEase() { EasingMode = EasingMode.EaseInOut };
             logWindow.BeginAnimation(DockPanel.WidthProperty, gridAnimation);
+        }
+
+        private void closeBtn_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            (DataContext as StartupViewModel).Credentials[1] = (sender as PasswordBox).Password;
         }
     }
 }
