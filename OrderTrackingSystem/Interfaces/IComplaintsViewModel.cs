5d784424463c6eb6d9cbed00c096f22986a8dd9d
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-27 21:02:15 +0200

    Added interface definitions

--- OrderTrackingSystem/Interfaces/IComplaintsViewModel.cs
+++ OrderTrackingSystem/Interfaces/IComplaintsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OrderTrackingSystem.Interfaces;
+using OrderTrackingSystem.Logic.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,19 @@ namespace OrderTrackingSystem.Presentation.Interfaces
 {
     interface IComplaintsViewModel
     {
+        List<ComplaintFolderDTO> ComplaintFolderList { get; set; }
+        List<ComplaintsDTO> ComplaintsList { get; set; }
+        List<ComplaintDefinitionDTO> ComplaintDefinitionList { get; set; }
+        ComplaintDefinitionDTO CurrentComplaint { get; set; }
+        List<ComplaintFolderDTO> AllComplaintFolderList { get; set; }
+        ComplaintFolderDTO SelectedFolder { get; set; }
+        ComplaintFolderDTO SelectedFolderToAdd { get; set; }
+        ComplaintsDTO SelectedComplaint { get; set; }
+
+        Task SetInitializeProperties();
+
+        RelayCommand AddTemplate { get; }
+        RelayCommand AddFolder { get; }
+        RelayCommand RemoveFolder { get; }
     }
 }
--- OrderTrackingSystem/Interfaces/IMailboxViewModel.cs
+++ OrderTrackingSystem/Interfaces/IMailboxViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OrderTrackingSystem.Interfaces;
+using OrderTrackingSystem.Logic.DTO;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,20 @@ namespace OrderTrackingSystem.Presentation.Interfaces
 {
     interface IMailboxViewModel
     {
+        MailDTO OriginalMail { get; set; }
+        CustomerDTO CurrentSender { get; set; }
+        List<MailDTO> ReceivedMessages { get; set; }
+        List<MailDTO> SentMessages { get; set; }
+        CustomerDTO MailReceiver { get; set; }
+        ObservableCollection<string> RelatedToCurrentMailOrders { get; set; }
+        List<OrderDTO> CustomerOrders { get; set; }
+        OrderDTO SelectedOrder { get; set; }
+        MailDTO SelectedMail { get; set; }
+
+        Task SetInitializeProperties();
+
+        RelayCommand FindReceiver { get; }
+        RelayCommand FilterCommand { get; }
+        RelayCommand SendMessage { get; }
     }
 }
