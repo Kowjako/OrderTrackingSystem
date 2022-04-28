bde6661912ae43d55075d99883752957cab96781
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-04-28 15:54:46 +0200

    Added CRUD abstract manager

---
+++ OrderTrackingSystem.Logic/Services/CRUDManager.cs
@@ -0,0 +1,40 @@
+﻿using OrderTrackingSystem.Logic.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Logic.Services
+{
+    public abstract class CRUDManager
+    {
+        public async virtual Task UpdateEntity<T>(T entity) where T : class
+        {
+            using(var dbContext = new OrderTrackingSystemEntities())
+            {
+                dbContext.Entry<T>(entity).State = System.Data.Entity.EntityState.Modified;
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async virtual Task DeleteEntity<T>(T entity) where T : class
+        {
+            using (var dbContext = new OrderTrackingSystemEntities())
+            {
+                dbContext.Entry<T>(entity).State = System.Data.Entity.EntityState.Deleted;
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+
+        public async virtual Task AddEntity<T>(T entity) where T : class
+        {
+            using (var dbContext = new OrderTrackingSystemEntities())
+            {
+                dbContext.Entry<T>(entity).State = System.Data.Entity.EntityState.Added;
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
