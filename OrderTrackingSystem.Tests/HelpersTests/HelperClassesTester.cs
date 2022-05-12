056467e04cba1ae2b0f55b74ab2e2b0c17f6cb2c
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-05-12 19:36:37 +0200

    Added xUnit project to solution for further tests

---
+++ OrderTrackingSystem.Tests/HelpersTests/HelperClassesTester.cs
@@ -0,0 +1,24 @@
+﻿using OrderTrackingSystem.Logic.EnumMappers;
+using OrderTrackingSystem.Logic.HelperClasses;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OrderTrackingSystem.Tests.HelpersTests
+{ 
+
+    public class HelperClassesTester
+    {
+        [Fact]
+        public void Cryptography_Encrypt_Decrypt()
+        {
+            var password = "QWERYIOPASDGJKLCXZVB<NM!@#%#^&*)(12415425216713";
+            var encrypted = Cryptography.EncryptWithRSA(password);
+
+            var decrypted = Cryptography.DecryptFromRSA(encrypted);
+            Assert.Equal(password, decrypted);
+        }
+
+    }
+}
