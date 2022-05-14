efdfa8c68231c718200204aa0bcfcd48827ccf8e
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-05-14 17:55:14 +0200

    Worked custom extensibleViewPanel content control

--- OrderTrackingSystem/CustomControls/ExtensibleViewPanel.xaml.cs
+++ OrderTrackingSystem/CustomControls/ExtensibleViewPanel.xaml.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Markup;
 
 namespace OrderTrackingSystem.Presentation.CustomControls
 {
+    [ContentProperty(nameof(ViewContent))]
     public partial class ExtensibleViewPanel : ContentControl
     {
         #region Dependency properties
