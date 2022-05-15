5348022daf1933ab1c6460576088dfd24700b26f
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-05-15 12:32:22 +0200

    Add product validator

---
+++ OrderTrackingSystem.Logic/Validators/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OrderTrackingSystem.Logic.DataAccessLayer;
+
+namespace OrderTrackingSystem.Logic.Validators
+{
+    public class ProductValidator : AbstractValidator<Products>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).MinimumLength(1).MaximumLength(100).WithMessage("Nazwa powinna być długości [1-100]");
+            RuleFor(x => x.PriceNetto).GreaterThan(0m).WithMessage("Cena musi być większa od zera");
+            RuleFor(x => x.VAT).InclusiveBetween((byte)0, (byte)23).WithMessage("Stawka VAT musi być od [0;23] %");
+            RuleFor(x => x.Weight).GreaterThan(0m).WithMessage("Waga musi być większa od zera");
+            RuleFor(x => x.Discount).GreaterThanOrEqualTo((byte)0).WithMessage("Zniżka nie może być ujemna");
+        }
+    }
+}
