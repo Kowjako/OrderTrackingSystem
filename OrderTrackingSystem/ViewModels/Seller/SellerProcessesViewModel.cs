72fbe09778dabc84c4061375ca10749fff19858b
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-23 13:14:12 +0200

    First stage of SellerProcess view implementation

---
+++ OrderTrackingSystem/ViewModels/Seller/SellerProcessesViewModel.cs
@@ -0,0 +1,45 @@
+﻿using OrderTrackingSystem.Presentation.Interfaces.Seller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Presentation.ViewModels.Seller
+{
+    public class SellerProcessesViewModel : ISellerProcessesViewModel, INotifyPropertyChanged
+    {
+        public SellerProcessesViewModel()
+        {
+
+        }
+
+        #region INotifyableViewModel implementation
+
+        public event Action<string> OnSuccess;
+        public event Action<string> OnFailure;
+        public event Action<string> OnWarning;
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void OnManyPropertyChanged(IEnumerable<string> props)
+        {
+            foreach (var prop in props)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        #endregion
+
+    }
+}
