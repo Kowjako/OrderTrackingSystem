3330198e0cf5f972bcf4767ee1fed71eee34339d
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-23 13:12:53 +0200

    Add process DTO for custom seller processes

---
+++ OrderTrackingSystem.Logic/DTO/ProcessDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Logic.DTO
+{
+    public class ProcessDTO
+    {
+        public string Name { get; set; }
+        public DateTime LastProcessDate { get; set; }
+        public string Description { get; set; }
+
+        public ProcessDTO(string procedure)
+        {
+            StoredProcedureFunction = procedure;
+        }
+
+        public readonly string StoredProcedureFunction;
+    }
+}
