f5442729e09c3fb540b93f50ccad41ae40730960
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-06-28 18:07:16 +0200

    Added CustomerSelectionViewModel and injected to CustomerSelectionView

---
+++ OrderTrackingSystem/CustomControls/Classes/CustomerSelectionViewModel.cs
@@ -0,0 +1,51 @@
+﻿using OrderTrackingSystem.Logic.DTO;
+using OrderTrackingSystem.Logic.Services;
+using OrderTrackingSystem.Logic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingSystem.Presentation.CustomControls.Classes
+{
+    public class CustomerDictionary
+    {
+        public CustomerDTO Customer { get; set; }
+        public bool IsChecked { get; set; }
+    }
+
+    public class CustomerSelectionViewModel : INotifyPropertyChanged
+    {
+        private static List<CustomerDictionary> _allCustomers = new List<CustomerDictionary>();
+
+        public List<CustomerDictionary> AllCustomers
+        {
+            get => _allCustomers;
+            set => _allCustomers = value;
+        }
+
+        private readonly ICustomerService CustomerService;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CustomerSelectionViewModel()
+        {
+            CustomerService = new CustomerService();
+        }
+
+        public async void LoadData()
+        {
+            var customerList = await CustomerService.GetAllCustomers();
+            customerList.ForEach(p => _allCustomers.Add(new CustomerDictionary { Customer = p, IsChecked = false }));
+            RaisePropertyChanged(nameof(AllCustomers));
+        }
+
+        public void RaisePropertyChanged(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        public bool HasData => _allCustomers != null && _allCustomers.Any();
+
+        public List<CustomerDTO> SelectedCustomers => _allCustomers.Where(p => p.IsChecked).Select(p => p.Customer).ToList();
+    }
+}
--- OrderTrackingSystem/CustomControls/CustomerSelection.xaml.cs
+++ OrderTrackingSystem/CustomControls/CustomerSelection.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using OrderTrackingSystem.Logic.DTO;
+using OrderTrackingSystem.Logic.Services;
+using OrderTrackingSystem.Logic.Services.Interfaces;
+using OrderTrackingSystem.Presentation.CustomControls.Classes;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrderTrackingSystem.Presentation.CustomControls
 {
@@ -19,10 +11,20 @@ namespace OrderTrackingSystem.Presentation.CustomControls
     /// Interaction logic for CustomerSelection.xaml
     /// </summary>
     public partial class CustomerSelection : UserControl
-    {
+    { 
         public CustomerSelection()
         {
             InitializeComponent();
+        }
+
+        public CustomerSelection(CustomerSelectionViewModel vm) : this() /* trzeba wywolac InitializeComponent() */
+        {
+            DataContext = vm;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WindowExtension.WindowExtension.ActualPopupWindow.Close();
         }
     }
 }
