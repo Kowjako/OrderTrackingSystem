173bb65575d93157743a714f6e1722236070d063
Wlodzimierz <kowyako@yandex.ru>
2022-07-10 12:02:37 +0200

    Added class test & fixture for Order Service

---
+++ OrderTrackingSystem.Tests/ClassFixtures/OrderTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using OrderTrackingSystem.Logic.Services;
+using OrderTrackingSystem.Logic.Services.Interfaces;
+using OrderTrackingSystem.Tests.ObjectFactory;
+using System;
+
+namespace OrderTrackingSystem.Tests.ClassFixtures
+{
+    public class OrderTestFixture : IDisposable
+    {
+        public EntitiesGenerator EntitiesGenerator;
+        public IOrderService OrderService;
+
+        public OrderTestFixture()
+        {
+            EntitiesGenerator = new EntitiesGenerator();
+            OrderService = new OrderService();
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ OrderTrackingSystem.Tests/ServicesTests/OrdersTests.cs
@@ -0,0 +1,19 @@
+﻿using OrderTrackingSystem.Tests.ClassFixtures;
+using OrderTrackingSystem.Tests.DatabaseFixture;
+using Xunit;
+
+namespace OrderTrackingSystem.Tests.ServicesTests
+{
+    [Collection("DBCollection")]
+    public class OrdersTests : IClassFixture<OrderTestFixture>
+    {
+        DBFixture db;
+        OrderTestFixture context;
+
+        public OrdersTests(DBFixture db, OrderTestFixture fixture)
+        {
+            this.db = db;
+            context = fixture;
+        }
+    }
+}
